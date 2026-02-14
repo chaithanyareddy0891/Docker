3e37e0c08cd310a22ee9cf3eb312f6ce5bba4b85
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2026-02-14 12:00:55 +0530

    adding instruction

--- install_docker_amazon_linux_2023.sh
+++ install_docker_amazon_linux_2023.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 # Amazon Linux 2023
 USER=$(whoami)
 sudo dnf install docker -y
 sudo systemctl enable docker --now
-sudo dnf install git -y
+# sudo dnf install git -y
 sudo usermod -aG docker $USER
